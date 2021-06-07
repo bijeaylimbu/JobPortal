0e2f3b4b26a1e2b4dc9e5eb873adb5a4c270818f
Bijay Limbu <bijaylimbu11111@gmail.com>
2021-06-08 05:12:05 +0545

    Basic setup and Navbar

---
+++ src/Page/Home/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from "../../Widget/Header/Header";
+
+class Home extends Component{
+
+
+
+    render(){
+
+
+        return (
+<>
+<Header/>
+</>
+
+
+
+        );
+
+    }
+}
+export default Home;
---
+++ src/Widget/NavBar/Navbar.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { AiFillCloseCircle as CloseMenu } from "react-icons/ai";
+import { FiMenu as MenuIcon } from "react-icons/fi";
+import  Logo  from "../../assets/Image/logo.png";
+import "../NavBar/Navbar.css";
+
+ 
+const Navbar=()=>{
+
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+
+        return (
+<>
+<div className="header">
+      <div className="logo-nav">
+        <div className="logo-container">
+          <a href="#">
+           <img src={Logo} className="logo"/>
+          </a>
+        </div>
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">Job</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">Companies</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">BLOG</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">About</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="#">SIGN-IN</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="" className="sign-up">
+              SIGN-UP
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul className="signin-up">
+        <li className="sign-in" onClick={closeMobileMenu}>
+          <a href="#">SIGN-IN</a>
+        </li>
+        <li onClick={closeMobileMenu}>
+          <a href="" className="signup-btn">
+            SIGN-UP
+          </a>
+        </li>
+      </ul>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
+    </div>
+</>
+
+        );
+    
+}
+
+export default Navbar;
